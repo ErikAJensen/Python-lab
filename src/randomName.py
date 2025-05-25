18ee9ee1e45e22c72e99388760ac8eb785cc58db
ErikAJensen <111729359+ErikAJensen@users.noreply.github.com>
2025-05-25 11:58:27 +0200

    Update randomName.py

--- src/randomName.py
+++ src/randomName.py
@@ -1,9 +1,9 @@
-import random
+import random  # Importerer random-modulen for å velge tilfeldig navn
 
 def random_navn():
-    navn_liste = ["Erik", "Nathalie"]
-    resultat = random.choice(navn_liste)
-    print("du fikk navnet", resultat)
+    navn_liste = ["Erik", "Nathalie", "Bernt" , "Roger"]  # Lager en liste med navn
+    resultat = random.choice(navn_liste)  # Velger et tilfeldig navn fra listen
+    print("du fikk navnet", resultat)  # Skriver ut det tilfeldige navnet
 
-input("Trykk enter for å få tildelt et random navn")
-random_navn()
+input("Trykk enter for å få tildelt et random navn")  # Venter på at bruker trykker enter
+random_navn()  # Kaller funksjonen for å vise et tilfeldig navn
