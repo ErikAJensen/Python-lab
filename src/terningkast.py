e6d1d1cb120524427f7e54286f776cb0e81f9ed2
ErikAJensen <111729359+ErikAJensen@users.noreply.github.com>
2025-05-25 12:03:45 +0200

    Kommenterer kode

--- src/terningkast.py
+++ src/terningkast.py
@@ -1,9 +1,9 @@
-import random
+import random  # Importerer random-modulen
 
 def kast_terning():
-    resultat = random.randint(1, 6)
-    print("du kastet", resultat)
+    resultat = random.randint(1, 6)  # Trekker et tilfeldig tall mellom 1 og 6
+    print("du kastet", resultat)  # Skriver ut resultatet
 
 
-input("Trykk enter for å kaste terningen")
-kast_terning()
+input("Trykk enter for å kaste terningen")  # Venter på at bruker trykker enter
+kast_terning()  # Kaller funksjonen for å kaste terningen
