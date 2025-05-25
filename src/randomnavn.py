2b98da8f15770c2afb332905f80cc412f6e76e5c
ErikAJensen <111729359+ErikAJensen@users.noreply.github.com>
2025-05-25 12:36:15 +0200

    Update randomnavn.py

--- src/randomnavn.py
+++ src/randomnavn.py
@@ -3,8 +3,18 @@ import random
 
 navnliste = ["Emma", "Oliver", "Nora", "Jakob", "Ella", "Liam", "Selma", "Aksel", "Sofie", "Isak", "Erik"]
 
+etternavn = ["Hansen", "Johansen", "Olsen", "Larsen", "Andersen",
+    "Pedersen", "Nilsen", "Kristiansen", "Jensen", "Karlsen",
+    "Johnsen", "Pettersen", "Eriksen", "Berg", "Haugen",
+    "Dahl", "Moen", "Solberg", "Moe", "Strand",
+    "Knutsen", "Andreassen", "Paulsen", "Lie", "Halvorsen"]
+
+
 def velg_navn():
    return random.choice(navnliste)
+
+def velg_etternavn():
+   return random.choice(etternavn)
 
 
 while True:
@@ -12,5 +22,4 @@ while True:
    if svar.lower() == "q":
       print("Ferdig")
       break
-   print("Ditt nye navn er:", velg_navn())
-   
+   print("Ditt nye navn er:", velg_navn(), velg_etternavn())
